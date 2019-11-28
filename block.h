5f207351d96e45d86f3cb74914cda987b6f6076b
Brad <sw27park@edu.uwaterloo.ca>
2019-11-28 02:47:22 -0500

    Implemented basic Block class

---
+++ block.h
@@ -0,0 +1,16 @@
+#include <string>
+
+class Block {
+    int orientation;
+    Grid grid;
+    Cell cells[4];
+    int xPos;
+    int yPos;
+    public:
+    virtual void clockWise() = 0;
+    virtual void counterClockWise() = 0;
+    void left();
+    void right();
+    void down();
+    void drop();
+}
