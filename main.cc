307b53fba358fb29dd16f9731362ffcfa6abfb97
bduge <billduge11@gmail.com>
2019-11-30 00:27:20 -0500

    Setup player grids in main

--- main.cc
+++ main.cc
@@ -1,3 +1,6 @@
+#include "grid.h"
+#include "level.h"
+#include "block.h"
 #include <iostream>
 #include <string>
 using namespace std;
@@ -6,16 +9,37 @@ int main(int argc, char* argv[]) {
 
     string cmd;
     int difficulty;
-    Grid playerOne = Grid();
-    Grid playerTwo = Grid();
+    TextDisplay *display = new TextDisplay();
+    Grid *playerOne = new Grid(1, display);
+    playerOne->init();
+    Grid *playerTwo = new Grid(2, display);
+    playerTwo->init();
+    string playerOneFile = "sequence1.txt";
+    string playerTwoFile = "sequence2.txt";
     bool playerOneTurn = true;
     
     cout << "Player 1, choose your difficulty between 0 and 4: ";
     cin >> difficulty;
     if (difficulty = 0){
-        
-        playerOne.setLevel(0);
+        Level *level = new LevelZero(playerOneFile);
+        playerOne->setLevel(0, level);
+    } else if (difficulty = 1){
+        Level *level = new LevelOne(playerOneFile);
+        playerOne->setLevel(0, level);
     }
+    // add more levels
+    cout << endl;
+
+    cout << "Player 2, choose your difficulty between 0 and 4: ";
+    cin >> difficulty;
+    if (difficulty = 0){
+        Level *level = new LevelZero(playerTwoFile);
+        playerTwo->setLevel(0, level);
+    } else if (difficulty = 1){
+        Level *level = new LevelOne(playerTwoFile);
+        playerTwo->setLevel(0, level);
+    }
+    // add more levels
     cout << endl;
 
     while (cin >> cmd){
