0d3b813f5dfbb51112d4da77b23c302f9d898c6e
bduge <billduge11@gmail.com>
2019-11-29 23:58:27 -0500

    begin working on main

--- main.cc
+++ main.cc
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    string cmd;
+    int difficulty;
+    Grid playerOne = Grid();
+    Grid playerTwo = Grid();
+    bool playerOneTurn = true;
+    
+    cout << "Player 1, choose your difficulty between 0 and 4: ";
+    cin >> difficulty;
+    if (difficulty = 0){
+        
+        playerOne.setLevel(0);
+    }
+    cout << endl;
+
+    while (cin >> cmd){
+        
+    }
 }
