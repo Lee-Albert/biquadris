1acdb64fd349dc2327973243ab9a49ee2684b90f
bduge <billduge11@gmail.com>
2019-12-01 21:14:31 -0500

    adding constructor for block subclasses

--- jblock.h
+++ jblock.h
@@ -1,9 +1,15 @@
+#ifndef JBLOCK_H
+#define JBLOCK_H
 #include "block.h"
+#include "grid.h"
+#include "tile.h"
 
 class JBlock : public Block {
     string name;
     public:
     void clockWise() override;
     void counterClockWise() override;
-    JBlock();
+    JBlock(int orientation, Grid *grid, Tile tiles, int xPos, int yPos);
 }
+
+#endif
--- lblock.cc
+++ lblock.cc
@@ -1,4 +1,7 @@
 #include "lblock.h"
+#include "block.h"
+#include "grid.h"
+#include "tile.h"
 
 void LBlock::clockWise(){
 
@@ -8,4 +11,10 @@ void LBlock::counterClockWise(){
 
 }
 
-LBlock::LBlock(): name{"L"}{};
+LBlock::LBlock(int orientation, Grid *grid, Tile tiles, int xPos, int yPos): 
+Block(orientation, grid, tiles, xPos, yPos), name{"L"}{
+    tiles[0] = &grid.at(3).at(0);
+    tiles[1] = &grid.at(3).at(1);
+    tiles[2] = &grid.at(3).at(2);
+    tiles[3] = &grid.at(2).at(2);
+}
